6819eef53d24043de37c961a1fc7f79ab4d6737f
Alan Perez <alanp9six@gmail.com>
2019-07-16 17:54:38 -0500

    Initial Commit: Setting up the structure for Stack, Queue, and the Driver class that will bee calling these two classes.

---
+++ src/Stack.java
@@ -0,0 +1,40 @@
+/**
+ * DESCRIPTION
+ * USAGE
+ *
+ * @author Alan Perez
+ * @version 1.0
+ */
+
+public class Stack {
+
+    public int size;
+    public int top;
+    public int[] data;
+
+    public Stack() {
+        this.size = 0;
+        this.top = -1;
+        this.data = null;
+    }
+
+    public Stack(int size) {
+        this.size = size;
+        this.top = -1;
+        this.data = new int[size];
+    }
+    // Push Method
+
+    // Pop Method
+
+    // showAll output value from stack
+
+    // Empty Reinitialize the stack to the empty stack
+
+    //isEmpty Test if the stack is empty (underflow condition)
+
+    // Peek
+
+    // Find
+
+ }
